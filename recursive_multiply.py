1f9b0ed0a12c3e88509053df5428479e124201fa
JiyanBlack <rglsm7655558@gmail.com>
2016-10-30 20:48:08 +0800

    add recursive_multiply implementation

---
+++ recursive_multiply.py
@@ -0,0 +1,20 @@
+def recursive_multiply(a, b):
+    '''
+    fast multiply a and b which have same even number digits
+    '''
+    digit_length = len(str(a))
+    a_str = str(a)
+    b_str = str(b)
+    divide_index = len(a_str) // 2
+    a1 = int(a_str[:divide_index])
+    a2 = int(a_str[divide_index:])
+    b1 = int(b_str[:divide_index])
+    b2 = int(b_str[divide_index:])
+    # print(a1, a2, b1, b2)
+    a1b1 = a1 * b1
+    a2b2 = a2 * b2
+    ad_bc = (a1 + a2) * (b1 + b2) - a1b1 - a2b2
+    return 10**digit_length * a1b1 + 10**(digit_length / 2) * ad_bc + a2b2
+
+print(recursive_multiply(3141592653589793238462643383279502884197169399375105820974944592,
+                         2718281828459045235360287471352662497757247093699959574966967627))
