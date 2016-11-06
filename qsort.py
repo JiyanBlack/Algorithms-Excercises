0ba3d5554c07aada16b5802d3446b1ee237e4f25
JiyanBlack <rglsm7655558@gmail.com>
2016-11-06 17:09:34 +0800

    qsort

---
+++ qsort.py
@@ -0,0 +1,51 @@
+import random
+
+sample_data = []
+with open('IntegerArray.txt', 'r') as f:
+    for line in f:
+        sample_data.append(int(line))
+
+
+def qsort(array):
+    compare_number = 0
+
+    def qsort_recursion(array, start, last):
+        nonlocal compare_number
+        compare_number += last - start - 1
+        array_len = last - start
+        if array_len > 1:
+            # pivot = start + int(random.random() * array_len)
+            # pivot = start
+            medium = start + array_len // 2 - \
+                1 if array_len % 2 == 0 else (start + last - 1) // 2
+            if array[start] > array[last - 1]:
+                if array[medium] > array[start]:
+                    pivot = start
+                else:
+                    if array[medium] > array[last - 1]:
+                        pivot = medium
+                    else:
+                        pivot = last - 1
+            else:
+                if array[medium] > array[last - 1]:
+                    pivot = last - 1
+                else:
+                    if array[medium] > array[start]:
+                        pivot = medium
+                    else:
+                        pivot = start
+            array[start], array[pivot] = array[pivot], array[start]
+            divider = start + 1
+            for i in range(start + 1, last):
+                if array[i] < array[start]:
+                    array[divider], array[i] = array[i], array[divider]
+                    divider += 1
+            array[start], array[divider - 1] = array[divider - 1], array[start]
+            qsort_recursion(array, start, divider - 1)
+            qsort_recursion(array, divider, last)
+
+    qsort_recursion(array, 0, len(array))
+    return compare_number
+
+print(qsort(sample_data))
+# print(sample_data)
